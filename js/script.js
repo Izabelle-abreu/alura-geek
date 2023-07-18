8931e120997403c48697d72f1dfb10494a0bfd76
Izabelle-abreu <zbllbr@gmail.com>
2023-07-18 13:39:46 -0300

    Criando o script do pojeto e algumas alterações

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const conexao = 'http://localhost:3000/starWars';
+
+async function buscarProdutosDaApi() {
+    const resposta = await fetch(conexao)
+    .then((req) => req.json())
+    .then((data) => mostraProdutos(data));
+
+    return resposta;
+}
+
+buscarProdutosDaApi();
+
+const elementoParaInserirProduto = document.querySelector('[data-produtos]');
+function mostraProdutos(produtos){
+    elementoParaInserirProduto.innerHTML = '';
+    produtos.forEach(produto => {
+        elementoParaInserirProduto.innerHTML += `
+    <div>
+        <img src="${produto.imagem}" alt="">
+        <h2 class="lista--titulo">${produto.nome}</h2>
+        <p class="lista--preco">R$${produto.preco}</p>
+        <a href="#" class="lista--link">Ver produto</a>
+    </div>
+    `
+    });
+
+    
+}
