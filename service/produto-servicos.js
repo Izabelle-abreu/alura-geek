0ebe40c01663d39df414ce6916d593ba41ada699
Izabelle-abreu <zbllbr@gmail.com>
2023-08-31 07:30:48 -0300

    adicionando as funções de alterar e deletar

--- service/produto-servicos.js
+++ service/produto-servicos.js
@@ -2,7 +2,14 @@
 const listaProduto = () => {
     return fetch('http://localhost:3000/produtos')
     .then((resposta) => resposta.json())
-    .catch((error) => console.log(error))
+    .catch((error) => console.log(error));
+}
+
+const listarUmProduto = (id) => {
+    return fetch(`http://localhost:3000/produtos/${id}`)
+    .then((resposta) => {
+        return resposta.json();
+    });
 }
 
 //POST
@@ -26,7 +33,39 @@ const criaProduto = (imagem, nome, preco) => {
     })
 }
 
+//PUT/PATCH
+const alteraProduto = async (id, nome, preco, descricao) => {
+    return fetch(`http://localhost:3000/produtos/${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            nome,
+            preco,
+            descricao,
+        }),
+    })
+    .then((resposta) => {
+        return resposta.json();
+    })
+    .catch((error) => console.log(error));
+}
+
+//DELETE
+const deleteProduto = async (id) => {
+    return await fetch(`http://localhost:3000/produtos/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+}
+
 export const produtoServices = {
     listaProduto,
     criaProduto,
+    listarUmProduto,
+    alteraProduto,
+    deleteProduto,
 }
